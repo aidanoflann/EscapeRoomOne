486080a0a82814d2b4bdd3fcc2917b71ea80cc51
Aidan <aidnoflann@gmail.com>
2017-06-11 04:26:39 +0100

    triggering painting 3Dification from button press (lazy method for now)

--- Assets/Scripts/e_button.cs
+++ Assets/Scripts/e_button.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class e_button : MonoBehaviour {
+public class e_button : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject Painting;
+	private SphereCollider collider;
+	
+	void Awake()
+	{
+		collider = GetComponent<SphereCollider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+	
+	#region TriggerMethods
+	public void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("EBUTTONPRESSED");
+		this.Painting.SetActive(false);
+	}
+	#endregion
+	
+	
 }
