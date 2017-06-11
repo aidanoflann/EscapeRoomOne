fd54c882a34363ae04691a605bb36c4d260de2d7
Aidan <aidanoflann@gmail.com>
2017-06-11 16:47:49 +0100

    missed cs

---
+++ Assets/Scripts/EscapeRoomButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeRoomButton : MonoBehaviour
+{
+
+	private PasswordManager _passwordManager;
+	public char Letter;
+
+	void Start()
+	{
+		this._passwordManager = this.gameObject.GetComponentInParent<PasswordManager> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	#region TriggerMethods
+	public void OnTriggerEnter(Collider other)
+	{
+		this._passwordManager.EnterLetter (this.Letter);
+	}
+	#endregion
+	
+	
+}
